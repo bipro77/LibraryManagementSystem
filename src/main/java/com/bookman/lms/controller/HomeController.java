945119147e2080ddd091e1fe1f7ab5f0c3de3a43
Biprojit Mondal <bipro77@gmail.com>
2025-05-19 16:44:14 +0600

    1. HomeController - index html added 2. application.properties - a) page suffix & prefix added b) Manual server port number added http://localhost:8080/

--- src/main/java/com/bookman/lms/controller/HomeController.java
+++ src/main/java/com/bookman/lms/controller/HomeController.java
@@ -1,16 +1,16 @@
 package com.bookman.lms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
-
-    @RequestMapping(value = { "/" }, method = RequestMethod.GET)
-    public String home() {
-            return "user/index.html";
+    //@RequestMapping("/")
+   // @RequestMapping(value = { "/","/index" }, method = RequestMethod.GET)
+    @GetMapping(value={"/","/index" })
+    public String index() {
+            return "index";
     }
-
 }
