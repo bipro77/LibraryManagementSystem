d9f56407131e5bf680a97a75f986099250ffe96d
Utshab Roy <utshab.roy@gmail.com>
2025-05-21 23:56:03 +0900

    CommandLineRunner added in the main func for testing

--- src/main/java/com/bookman/lms/LibraryManagementSystemApplication.java
+++ src/main/java/com/bookman/lms/LibraryManagementSystemApplication.java
@@ -1,14 +1,28 @@
 package com.bookman.lms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+//import com.bookman.lms.entity.User;
 
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+
 	}
+
+    @Bean
+    CommandLineRunner demo() {
+	    return (args) -> {
+//	    	User user = new User("abc", "abc@gmail.com", "passWOED");
+//	    	System.out.println(user);
+	    	System.out.println("App Started");
+	    };
+	  }
 
 
 }
