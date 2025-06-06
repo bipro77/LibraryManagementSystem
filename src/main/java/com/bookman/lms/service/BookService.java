e23956c91544ae291bcd17badb3d7662fcfdedf2
Biprojit Mondal <bipro77@gmail.com>
2025-06-06 13:54:25 +0600

    Reapply "GlobalExceptionHandler & comments restore"
    This reverts commit 438128d1921e9e2d719ccfdcd0464aeb6b87ba51.

--- src/main/java/com/bookman/lms/service/BookService.java
+++ src/main/java/com/bookman/lms/service/BookService.java
@@ -10,6 +10,10 @@ import com.bookman.lms.entity.Book;
 import com.bookman.lms.exception.ResourceNotFoundException;
 import com.bookman.lms.repository.BookRepository;
 
+/**
+ * Service layer for managing Book entities. Contains business logic and
+ * interacts with the BookRepository.
+ */
 @Service // Marks this class as a Spring service component
 public class BookService {
 
@@ -20,19 +24,44 @@ public class BookService {
 		this.bookRepository = bookRepository;
 	}
 
+	/**
+	 * Retrieves all books from the database.
+	 * 
+	 * @return A list of all books.
+	 */
 	public List<Book> getAllBooks() {
 		return bookRepository.findAll();
 	}
 
+	/**
+	 * Retrieves a book by its ID.
+	 * 
+	 * @param id The ID of the book to retrieve.
+	 * @return An Optional containing the Book if found, empty otherwise.
+	 */
 	public Optional<Book> getBookById(Long id) {
 		return bookRepository.findById(id);
 	}
 
+	/**
+	 * Creates a new book.
+	 * 
+	 * @param book The Book object to be created.
+	 * @return The saved Book object (with generated ID).
+	 */
 	@Transactional // Ensures the entire method executes as a single transaction
 	public Book createBook(Book book) {
 		return bookRepository.save(book);
 	}
 
+	/**
+	 * Updates an existing book.
+	 * 
+	 * @param id          The ID of the book to update.
+	 * @param updatedBook The Book object with updated information.
+	 * @return The updated Book object.
+	 * @throws BookNotFoundException if the book with the given ID is not found.
+	 */
 	@Transactional
 	public Book updateBook(Long id, Book updatedBook) {
 		return bookRepository.findById(id).map(existingBook -> {
@@ -44,6 +73,12 @@ public class BookService {
 		}).orElseThrow(() -> new ResourceNotFoundException("Book with ID " + id + " not found."));
 	}
 
+	/**
+	 * Deletes a book by its ID.
+	 * 
+	 * @param id The ID of the book to delete.
+	 * @throws ResourceNotFoundException if the book with the given ID is not found.
+	 */
 	@Transactional
 	public void deleteBook(Long id) {
 		if (!bookRepository.existsById(id)) {
@@ -52,6 +87,12 @@ public class BookService {
 		bookRepository.deleteById(id);
 	}
 
+	/**
+	 * Creates multiple new books.
+	 * 
+	 * @param books A list of Book objects to be created.
+	 * @return A list of the saved Book objects (with generated IDs).
+	 */
 	@Transactional
 	public List<Book> createMultipleBooks(List<Book> books) {
 		// saveAll method of JpaRepository efficiently saves a collection of entities
