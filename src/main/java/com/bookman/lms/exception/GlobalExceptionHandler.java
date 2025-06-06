d95b1c0f039ffb315f88bb6f04575ec428520341
Biprojit Mondal <bipro77@gmail.com>
2025-06-06 17:30:52 +0600

    unused import removed

--- src/main/java/com/bookman/lms/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bookman/lms/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.bookman.lms.exception;
 
-import java.io.StringWriter;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,8 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
